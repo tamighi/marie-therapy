9989be9a13990f8060a953d563264e228400718c
tamighi <thomas.amighi@hotmail.com>
2024-10-30 20:45:18 +0100

    style: fix icon

--- src/routes/ModalitiesPage/ModalityCard.tsx
+++ src/routes/ModalitiesPage/ModalityCard.tsx
@@ -6,8 +6,8 @@ type Props = Modality;
 export const ModalityCard = ({ title, content, icon }: Props) => {
     return (
         <Card className="flex flex-col gap-8 h-full">
-            <div className="flex gap-8 justify-between items-center">
-                <h3>{title}</h3>
+            <div className="flex gap-4 items-center">
+                <h3 className="font-bold">{title}</h3>
                 <img className="w-6 h-6" src={icon} />
             </div>
             <p>{content}</p>
